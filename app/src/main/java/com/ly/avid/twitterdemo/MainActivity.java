2fcbdb21fbb1a1c9499ffa23cdc04f0b70408ffe
Sober <15821589161@163.com>
2017-03-27 17:36:09 +0800

    修改文档显示格式，增加twitter登陆

--- app/src/main/java/com/ly/avid/twitterdemo/MainActivity.java
+++ app/src/main/java/com/ly/avid/twitterdemo/MainActivity.java
@@ -1,12 +1,21 @@
 package com.ly.avid.twitterdemo;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
 import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 import com.twitter.sdk.android.tweetcomposer.TweetComposer;
 
 import io.fabric.sdk.android.Fabric;
@@ -15,12 +24,15 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "UBESZRySevxEhZgPJRB5Z48Hi";
     private static final String TWITTER_SECRET = "0Rjr5L0kG9HBsAmeyVe2ZkPchrFW8imgB6htU00KnrkPnfqRuN";
 
+    private TwitterLoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +40,26 @@ public class MainActivity extends AppCompatActivity {
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
+
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // The TwitterSession is also available through:
+                // Twitter.getInstance().core.getSessionManager().getActiveSession()
+                TwitterSession session = result.data;
+                Log.i("xxxxx", "id: "+session.getId()+"userName: "+session.getUserName()+"hashCode: "+session.hashCode()+"AuthToken: "+session.getAuthToken().toString()+"UserId: "+session.getUserId());
+                // TODO: Remove toast and use the TwitterSession's userID
+                // with your app's user model
+                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d("TwitterKit", "Login with Twitter failure", exception);
+            }
+        });
+
 
         findViewById(R.id.btn_share).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,4 +74,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Make sure that the loginButton hears the result from any
+        // Activity that it triggered.
+        loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
